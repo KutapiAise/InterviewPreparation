8f720f61450286712c8907a6a7913d1668305226
Aishwarya A.S <ashkash.aishwarya@gmail.com>
2020-07-25 19:09:38 +0400

    Update and rename Questions-Set1.py to Questions-Set1.ipynb

--- Questions-Set1.py
+++
@@ -1,3 +0,0 @@
-list: Imagine you are looking for your socks in your closet, but you don't know in which drawer your socks are, so you have to search drawer by drawer until you find them (or maybe you never do). That's what we call O(n), because in the worst scenario, you will look in all your drawers (where n is the number of drawers).
-
-set: Now, imagine you're still looking for your socks in your closet, but now you know in which drawer your socks are, say in the 3rd drawer. So, you will just search in the 3rd drawer, instead of searching in all drawers. That's what we call O(1), because in the worst scenario you will look in just one drawer.
